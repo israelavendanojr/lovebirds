a23a32657fdd677e0da100d0c59b22afd64914d7
israelavendanojr <iajr0207@gmail.com>
2025-05-14 23:06:54 -0700

    Tailwind so cooked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ const App = () => {
     <div className="min-h-screen flex items-center justify-center text-center">
       <h1 className="text-5xl font-bold text-pink-300 font-['Dancing_Script']">
         Our Love Story Begins...
+        
       </h1>
     </div>
   );
